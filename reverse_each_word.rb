ee1ea918395e072aaf11b4193698ade6684be47f
Tristan Myers <tristifano@gmail.com>
2019-08-06 19:31:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
 def reverse_each_word(string)
+  string_array = string.split(" ") 
+  reverse_array = []
+  string_array.each { |word|
+  reverse_array.push(word.reverse)
+  }
+  reverse_array.join(" ")
+enddef reverse_each_word(string)
   string.split(" ") { |word_array|
     word_array.each { |word|
       print word
